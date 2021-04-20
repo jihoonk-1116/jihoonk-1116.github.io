089e5c01d7abc609cda5249de81471e2f80bdc49
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-04-20 18:59:48 -0400

    Update index.js

--- finalProject/js/index.js
+++ finalProject/js/index.js
@@ -1,6 +1,6 @@
 $(function () {
     $("#stu").click(function () {
-        $("#login-btn").attr("href","stu-overview.html")
+        $("#login-btn").attr("href","https://ganiasif98.github.io/Final/student_overview.html?_ijt=iekhh3jjcl1e1sn92tovk32nqf")
     });
     $("#inst").click(function () {
         $("#login-btn").attr("href","overview.html")
