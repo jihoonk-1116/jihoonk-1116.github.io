ceeb1e43c6990bbbc44f207c0da46cf9fbf94d2b
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-04-20 15:42:07 -0400

    Add JS

---
+++ finalProject/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#stu").click(function () {
+        $("#login-btn").attr("href","stu-overview.html")
+    });
+    $("#inst").click(function () {
+        $("#login-btn").attr("href","overview.html")
+    });
+});
+console.log("new");
