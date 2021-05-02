078ca1c1f6d5af0dc66bee88ba2774b58a3179de
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-05-01 20:44:42 -0400

    Create mongoose.js

---
+++ freecodecamp/mongoose.js
@@ -0,0 +1,5 @@
+/*
+1. Install and Set Up Mongoose
+*/
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://JihoonDB:6581139a@firstdb.irfsc.mongodb.net/firstDB?retryWrites=true&w=majority");
