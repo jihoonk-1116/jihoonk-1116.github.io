7b67ea84bb96e7d3feecdeb8d2406fa45c672396
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-05-01 20:57:59 -0400

    update question 2

--- freecodecamp/mongoose.js
+++ freecodecamp/mongoose.js
@@ -3,3 +3,15 @@
 */
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://JihoonDB:6581139a@firstdb.irfsc.mongodb.net/firstDB?retryWrites=true&w=majority");
+/*
+2. Make Person Schema
+*/
+const Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    name: { type: String, required: true },
+    age: Number,
+    favoriteFoods:[String]
+  });
+
+var Person = mongoose.model('Person',personSchema);
