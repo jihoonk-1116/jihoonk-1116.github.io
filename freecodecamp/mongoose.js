e460467d23b61ddb70bc587938505e2c95bfbd63
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-05-02 21:01:07 -0400

    Update mongoose.js

--- freecodecamp/mongoose.js
+++ freecodecamp/mongoose.js
@@ -15,3 +15,40 @@ var personSchema = new Schema({
   });
 
 var Person = mongoose.model('Person',personSchema);
+/*
+3. Create and Save a Record of a ModelPassed
+*/
+var Person = mongoose.model('Person',personSchema);
+
+const createAndSavePerson = function(done) {
+  const person = new Person({name: 'Joe', age: 21, favoriteFoods: ['pizza', 'burger']});
+  person.save((err, data) => {
+    if (err) {
+      done(err);
+    }
+    done(null, data);
+  });
+};
+/*
+4.
+Create Many Records with model.create()Passed
+*/
+var createManyPeople = function(arrayOfPeople, done) {
+    Person.create(arrayOfPeople, function (err, data) {
+      if (err) {
+        done(err);
+      }
+    done(null, data);
+    });
+};
+/*
+5
+*/
+var findPeopleByName = function(personName, done) {
+    Person.find({name:personName}, function (err, data) {
+    if(err){
+      return done(err);
+    }
+    return done(null, data);
+    })
+};
