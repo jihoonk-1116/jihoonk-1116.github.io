2c4ed254f743fb8d9edc36d8c1fb9378c8d8f82e
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-05-09 21:18:21 -0400

    Delete finalProject directory

--- finalProject/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#stu").click(function () {
-        $("#login-btn").attr("href","https://ganiasif98.github.io/Final/student_overview.html");
-    });
-    $("#inst").click(function () {
-        $("#login-btn").attr("href","overview.html");
-    });
-});
-console.log("new");
